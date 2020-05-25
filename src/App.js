7e8b29777e0279d311b8d85b0b954ef04a9bff8d
adeng.y <357161031@qq.com>
2020-05-26 00:55:55 +0800

    添加插件antd-mobile,node-sass,router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+import Home from './pages/home'
+import About from './pages/cart'
+import Detail from './pages/detail'
+
+
+const App = () => (
+  <Router>
+    <div className="mod-pages">
+      <Route exact path="/" component={Home}></Route>
+      <Route path="/detail" component={Detail}></Route>
+      <Route path="/about" component={About}></Route>
     </div>
-  );
-}
+  </Router>
+)
 
-export default App;
+export default App
