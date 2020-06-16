46cd239724e9d183a3d9666ee700cf3fd73882db
adeng.y <357161031@qq.com>
2020-06-16 22:53:49 +0800

    fix购物车全选 ---》还有bug

--- src/component/tabbar/tabbar.js
+++ src/component/tabbar/tabbar.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import './tabbar.scss'
 
 class TabBarItem extends Component{
+  state ={
+    cartVal: 0
+  }
+  componentDidMount(){
+    console.log(JSON.parse(localStorage.getItem('cart_history')))
+    this.setState({cartVal: JSON.parse(localStorage.getItem('cart_history')).length})
+  }
   render(){
     return (
       <div className="tab-bar">
         <ul>
           <li><a href="/search"><i className="iconfont icon-sousuo1"></i>搜索</a></li>
           <li><a href="/sort"><i className="iconfont icon-fenlei"></i>分类</a></li>
-          <li><a href="/cart"><i className="iconfont icon-gouwuche"></i>购物车</a></li>
+          <li><a href="/cart"><i className="iconfont icon-gouwuche"><em>{this.state.cartVal}</em></i>购物车</a></li>
           <li><a href="/account/my"><i className="iconfont icon-wode"></i>我的</a></li>
         </ul>
       </div>
--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -8,8 +8,6 @@ import './cart.scss'
 import HotProductItem from '../../component/hotProduct'
 
 const CheckboxItem = Checkbox.CheckboxItem;
-const RadioItem = Checkbox.AgreeItem;
-
 
 class Cart extends Component{
   constructor(props) {
@@ -17,10 +15,10 @@ class Cart extends Component{
     this.state = {
       ids: '',
       hot: [],
+      value: [],
+      allChechked: false,
+      TotalPriceVal: 0,
     }
-  }
-  onChangeBox = (val) => {
-    console.log(val);
   }
   componentDidMount(){
     document.title = config.CONFIG_TITLE.cart
@@ -47,28 +45,29 @@ class Cart extends Component{
             <ul>
               {this.state.hot.slice(0, this.state.ids.length).map((item,index)=>(
                 <li key={index}>
-                  <div className="chebox"><CheckboxItem onChange={() => this.onChangeBox(item.productId)} key={item.productId} value={item.productId}></CheckboxItem></div>
+                  <div className="chebox"><CheckboxItem onChange={() => this.onChangeBox(index)} checked={item.checked}></CheckboxItem></div>
                   <div className="imgbox"><img src={item.products.coverUrl} /></div>
                   <div className="cnt">
-                    <div className="name"><span className="t">{item.products.name}</span><span className="cprice">￥11</span></div>
+                    <div className="name"><span className="t">{item.products.name}</span><span className="cprice">￥{item.products.minPrice}</span></div>
                     <div className="sku">
                       <span className="color">{item.skuArr}</span>
                       <div className="num">
                         <span className="btn dis"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 62 60"><path d="m21.5 28h20c.828 0 1.5.672 1.5 1.5 0 .829-.672 1.5-1.5 1.5h-20c-.829 0-1.5-.671-1.5-1.5 0-.828.671-1.5 1.5-1.5"></path></svg></span>
-                        <input type="number" value="1" />
+                        <input type="number" disabled value="1" />
                         <span className="btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 62 60"><path d="m41.5 28h-8.5v-8.5c0-.828-.672-1.5-1.5-1.5s-1.5.672-1.5 1.5v8.5h-8.5c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5h8.5v8.5c0 .828.672 1.5 1.5 1.5s1.5-.672 1.5-1.5v-8.5h8.5c.828 0 1.5-.671 1.5-1.5 0-.828-.672-1.5-1.5-1.5"></path></svg></span>
                       </div>
                     </div>
                   </div>
                 </li>
               ))}
+
             </ul>
           </div>
           <div className="tips">全场满￥199免运费，还差￥199</div>
           <div className="cart-bar">
-            {/* <div className="chebox"><CheckboxItem readOnly value="1">全选</CheckboxItem></div> */}
+            <div className="chebox"><CheckboxItem onChange={(e)=>this.onAllCheckBox(e)} checked={this.state.allChechked}>全选</CheckboxItem></div>
             <div className="inner">
-              <div className="cprice">合计：<span className="red">￥<em className="val">199</em></span></div>
+              <div className="cprice">合计：<span className="red">￥<em className="val">{this.TotalPrice()}</em></span></div>
               <div className="freight">差199免运费</div>
             </div>
             <div className="paybtn dis">结算</div>
@@ -82,9 +81,58 @@ class Cart extends Component{
           <HotProductItem title="猜你喜欢"  hotProductData = {this.state.hot}  />
         </React.Fragment>
         }
-
       </div>
     )
+  }
+  //单选
+  onChangeBox = (index) => {
+    this.state.hot[index].checked = !this.state.hot[index].checked;
+
+    this.state.hot.slice(0, this.state.ids.length).some((item,i)=>
+      {
+        console.log(i,this.state.hot[index].checked);
+        
+        if(item.checked){
+          this.setState({allChechked: true})
+          console.log(111);
+        }else{
+          console.log(222);
+          this.setState({allChechked: false})
+        }
+      }
+    )
+    
+    
+    this.setState({
+      hot:this.state.hot
+    })
+
+  }
+  // 全选
+  onAllCheckBox(e){
+    console.log(e.target)
+    const checked = e.target.checked
+    this.setState({
+      allChechked: checked
+    })
+    if (checked) {
+      this.state.hot.slice(0, this.state.ids.length).forEach(i=>{
+        i.checked = true;
+      })
+    } else {
+      this.state.hot.slice(0, this.state.ids.length).forEach(i=>{
+        i.checked = false;
+      })
+    }
+    this.setState({ // 每点击一次更新状态
+      hot:this.state.hot
+    })
+  }
+  // 获取总价
+  TotalPrice(){
+    let a = 1,
+    b = 2;
+    return a+b
   }
 }
 
